a34d85103d42434f2ec8125d1975a5aca0cce8c7
Alex Lipovka <alex.lipovka@gmail.com>
2026-04-10 21:52:07 +0400

    move readonly boolean to src/readonly-controls

--- src/components/BaseQuestionnaireResponseForm/ReadonlyQuestionnaireResponseForm.tsx
+++ src/components/BaseQuestionnaireResponseForm/ReadonlyQuestionnaireResponseForm.tsx
@@ -15,6 +15,7 @@ import {
 } from 'src/components/BaseQuestionnaireResponseForm/context';
 import { Barcode } from 'src/controls/Barcode';
 import { AudioAttachment } from 'src/readonly-controls/AudioAttachment';
+import { QuestionBoolean } from 'src/readonly-controls/Boolean';
 import { Col, Group, Row } from 'src/readonly-controls/Group';
 import { NavigationGroup } from 'src/readonly-controls/Group/NavigationGroup';
 import { GroupWizardVertical } from 'src/readonly-controls/GroupWizard';
@@ -22,7 +23,6 @@ import { MarkdownCard, MarkdownDisplay, MarkdownRenderControl } from 'src/readon
 import { service } from 'src/services/fhir';
 import { evaluate } from 'src/utils/fhirpath';
 
-import { QuestionBoolean } from './readonly-widgets/boolean';
 import { QuestionChoice } from './readonly-widgets/choice';
 import { QuestionDateTime } from './readonly-widgets/date';
 import { Display } from './readonly-widgets/display';
