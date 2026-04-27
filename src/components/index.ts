499b79987845d27a4a2bb666dddb8ea9a3c1a826
Alex Lipovka <alex.lipovka@gmail.com>
2026-04-27 17:28:11 +0400

    remove ResourceTable component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 export * from './DatePicker';
 export * from './Modal';
-export * from './ResourceTable';
 export * from './SearchBar';
 export * from './SearchBar/hooks';
 export * from './Select';
