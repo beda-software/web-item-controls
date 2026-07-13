9c1d131f27b8c8f26a68f0b4870dc7f38c246a3f
Ilya Beda <ir4y.ix@gmail.com>
2026-07-13 13:09:09 +1000

    Use new design for GridGroup

--- src/controls/Group/GridGroup/Grid.styles.ts
+++ src/controls/Group/GridGroup/Grid.styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { Text } from 'src/components/Typography';
 
@@ -7,10 +7,22 @@ interface GridContainerProps {
 }
 
 export const S = {
+    Widget: styled.div`
+        width: 100%;
+        background-color: ${({ theme }) => theme.neutralPalette.gray_1};
+        border: 1px solid ${({ theme }) => theme.neutralPalette.gray_4};
+        border-radius: 10px;
+        overflow: hidden;
+    `,
+
     Header: styled.div`
         display: flex;
-        gap: 16px;
-        padding-right: calc(18px + 16px);
+        align-items: center;
+        gap: 10px;
+        padding: 12px;
+        background-color: ${({ theme }) => theme.neutralPalette.gray_3};
+        border-bottom: 1px solid ${({ theme }) => theme.neutralPalette.gray_4};
+        color: ${({ theme }) => theme.antdTheme?.colorText};
     `,
 
     Column: styled.div`
@@ -22,19 +34,45 @@ export const S = {
     GridContainer: styled.div<GridContainerProps>`
         display: grid;
         grid-template-columns: ${({ columns }) => `repeat(${columns || 'auto-fit'}, minmax(100px, 1fr))`};
-        gap: 10px;
+        align-items: stretch;
+    `,
+
+    GridHeaderCell: styled.div`
+        display: flex;
         align-items: center;
+        min-height: 44px;
+        padding: 12px 8px 13px;
+        background-color: ${({ theme }) => theme.neutralPalette.gray_2};
+        border-bottom: 1px solid ${({ theme }) => theme.neutralPalette.gray_4};
+        color: ${({ theme }) => theme.antdTheme?.colorText};
+        font-size: 14px;
+        line-height: 22px;
+        font-weight: 600;
     `,
 
     GridRowLabel: styled.div`
         display: flex;
         align-items: center;
-        font-weight: bold;
-        padding: 10px 0;
+        color: ${({ theme }) => theme.antdTheme?.colorText};
+        font-size: 16px;
+        font-weight: 600;
     `,
 
-    GridItem: styled.div`
+    GridItem: styled.div<{ $bold?: boolean }>`
         display: flex;
-        /* padding-right: calc(18px + 16px); */
+        align-items: center;
+        min-height: 56px;
+        padding: 12px;
+        background-color: ${({ theme }) => theme.neutralPalette.gray_1};
+        border-bottom: 1px solid ${({ theme }) => theme.neutralPalette.gray_4};
+        color: ${({ theme }) => theme.antdTheme?.colorText};
+        font-size: 14px;
+        line-height: 22px;
+
+        ${({ $bold }) =>
+            $bold &&
+            css`
+                font-weight: 600;
+            `}
     `,
 };
